c6b7b4438f3d5f37480f959b4af707331dbf9b44
Davi Haas Rodrigues <davihaasrodrigues@gmail.com>
2026-06-24 15:23:21 -0300

    add: solution for exercise 9

--- 0009-palindromeNumber/0009-palindromeNumber.go
+++ 0009-palindromeNumber/0009-palindromeNumber.go
@@ -3,13 +3,44 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 )
 
 func isPalindrome(x int) bool {
 
-	// Your logic here
+	// if x is negative, it cannot be palindrome
+	if x < 0 {
+		return false
+	}
 
-	return false
+	// if x is above or bellow 32-bit range, it is out of the constrains range
+	if x < -2147483648 || x > 2147483647 {
+		return false
+	}
+
+	//convert int to string
+	s := strconv.Itoa(x)
+
+	// palindrome numbers must always have equal starter and ending numbers, so we track both ends of the number togheter and compare them
+	for left, right := 0, len(s)-1; left < right; left, right = left+1, right-1 {
+
+		if (left-right) == 0 && s[left] == s[right] {
+			return true
+
+		}
+
+		if (left-right) == -1 && s[left] == s[right] {
+			return true
+
+		}
+
+		if s[left] != s[right] {
+			return false
+		}
+	}
+
+	//if gets here, then the number is a palindrome since all return false condition were never reached
+	return true
 }
 
 type testCase struct {
