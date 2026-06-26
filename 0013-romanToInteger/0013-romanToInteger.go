2d865c517102a85e4c6de597bf248694c846b7db
Davi Haas Rodrigues <davihaasrodrigues@gmail.com>
2026-06-26 17:19:10 -0300

    add: solution for exercise 13

--- 0013-romanToInteger/0013-romanToInteger.go
+++ 0013-romanToInteger/0013-romanToInteger.go
@@ -7,6 +7,89 @@ import (
 
 func romanToInt(s string) int {
 
+	invalid := 0
+	valid := 0
+
+	if s == "" || s == "0" {
+		return invalid
+	}
+
+	for i := 0; i < len(s); i++ {
+
+		switch s[i] {
+		case 'M':
+			valid += 1000
+
+		case 'D':
+			valid += 500
+
+		case 'C':
+			if i+1 >= len(s) {
+				valid = +100
+				continue
+			}
+			if s[i+1] == 'D' {
+				valid += 400
+				i++
+				continue
+			}
+			if s[i+1] == 'M' {
+				valid += 900
+				i++
+				continue
+			}
+
+			valid += 100
+
+		case 'L':
+			valid += 50
+
+		case 'X':
+			if i+1 >= len(s) {
+				valid = +10
+				continue
+			}
+			if s[i+1] == 'L' {
+				valid += 40
+				i++
+				continue
+			}
+			if s[i+1] == 'C' {
+				valid += 90
+				i++
+				continue
+			}
+			valid += 10
+
+		case 'V':
+			if i != 0 && s[i-1] == 'I' {
+				valid += 4
+				continue
+			}
+			valid += 5
+
+		case 'I':
+			if i+1 >= len(s) {
+				valid += 1
+				continue
+			}
+			if s[i+1] == 'V' {
+				valid += 4
+				i++
+				continue
+			}
+			if s[i+1] == 'X' {
+				valid += 9
+				i++
+				continue
+			}
+			valid += 1
+
+		}
+
+	}
+
+	return valid
 }
 
 type testCase struct {
