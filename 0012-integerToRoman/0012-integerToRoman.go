1417e3b99e200e53647b90fdca3440c5e5903ec7
Davi Haas Rodrigues <davihaasrodrigues@gmail.com>
2026-06-26 14:18:22 -0300

    add: correct solution for exercise 12

--- 0012-integerToRoman/0012-integerToRoman.go
+++ 0012-integerToRoman/0012-integerToRoman.go
@@ -3,33 +3,164 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 )
 
 func intToRoman(num int) string {
 
 	invalid := ""
+	currentStr := ""
 
-	// Constraints
 	if num < 1 || num > 3999 {
 		return invalid
 	}
 
-	// ======================================================
-	// Your solution starts here
-	//
-	// Suggested ideas:
-	//
-	// - Keep track of the remaining value.
-	// - Build the Roman numeral from left to right.
-	// - Handle thousands, hundreds, tens and ones.
-	// - Remember the six subtractive cases:
-	//     IV  IX
-	//     XL  XC
-	//     CD  CM
-	//
-	// ======================================================
+	numStr := strconv.Itoa(num)
 
-	return ""
+	multiplier := 1
+	for i := 1; i < len(numStr); i++ {
+		multiplier *= 10
+	}
+
+	for i := 0; i < len(numStr); i++ {
+
+		digit := int(numStr[i] - '0')
+		if digit == 4 {
+
+			if multiplier == 100 {
+				currentStr = currentStr + "CD"
+			}
+
+			if multiplier == 10 {
+				currentStr = currentStr + "XL"
+			}
+
+			if multiplier == 1 {
+				currentStr = currentStr + "IV"
+			}
+
+			multiplier /= 10
+			continue
+		}
+
+		if digit == 9 {
+
+			if multiplier == 100 {
+				currentStr = currentStr + "CM"
+			}
+
+			if multiplier == 10 {
+				currentStr = currentStr + "XC"
+			}
+
+			if multiplier == 1 {
+				currentStr = currentStr + "IX"
+			}
+
+			multiplier /= 10
+			continue
+		}
+		if digit == 5 {
+
+			if multiplier == 100 {
+				currentStr = currentStr + "D"
+			}
+
+			if multiplier == 10 {
+				currentStr = currentStr + "L"
+			}
+
+			if multiplier == 1 {
+				currentStr = currentStr + "V"
+			}
+
+			multiplier /= 10
+			continue
+		}
+		if digit > 5 && digit < 9 {
+
+			if multiplier == 100 {
+				add := "D"
+				digitNew := digit
+				for digitNew > 5 {
+					add = add + "C"
+					digitNew--
+				}
+				currentStr = currentStr + add
+			}
+
+			if multiplier == 10 {
+				add := "L"
+				digitNew := digit
+				for digitNew > 5 {
+					add = add + "X"
+					digitNew--
+				}
+				currentStr = currentStr + add
+			}
+
+			if multiplier == 1 {
+				add := "V"
+				digitNew := digit
+				for digitNew > 5 {
+					add = add + "I"
+					digitNew--
+				}
+				currentStr = currentStr + add
+			}
+
+			multiplier /= 10
+			continue
+		}
+		if digit < 4 {
+
+			if multiplier == 1000 {
+				add := ""
+				digitNew := digit
+				for digitNew > 0 {
+					add = add + "M"
+					digitNew--
+				}
+				currentStr = currentStr + add
+			}
+
+			if multiplier == 100 {
+				add := ""
+				digitNew := digit
+				for digitNew > 0 {
+					add = add + "C"
+					digitNew--
+				}
+				currentStr = currentStr + add
+			}
+
+			if multiplier == 10 {
+				add := ""
+				digitNew := digit
+				for digitNew > 0 {
+					add = add + "X"
+					digitNew--
+				}
+				currentStr = currentStr + add
+			}
+
+			if multiplier == 1 {
+				add := ""
+				digitNew := digit
+				for digitNew > 0 {
+					add = add + "I"
+					digitNew--
+				}
+				currentStr = currentStr + add
+			}
+
+			multiplier /= 10
+			continue
+		}
+
+	}
+
+	return currentStr
 }
 
 type testCase struct {
