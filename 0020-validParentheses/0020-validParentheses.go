e129c78ef6ab59c96d2b22aa2a61da21e288c170
Davi Haas Rodrigues <davihaasrodrigues@gmail.com>
2026-07-02 13:29:18 -0300

    add: solution for exercise 20

--- 0020-validParentheses/0020-validParentheses.go
+++ 0020-validParentheses/0020-validParentheses.go
@@ -6,6 +6,43 @@ import (
 
 func isValid(s string) bool {
 
+	if len(s)%2 == 1 {
+		return false
+	}
+
+	stack := []byte{}
+
+	for i := 0; i < len(s); i++ {
+
+		switch s[i] {
+
+		case '(', '[', '{':
+
+			stack = append(stack, s[i])
+
+		case ')':
+			if len(stack) == 0 || stack[len(stack)-1] != '(' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+
+		case ']':
+			if len(stack) == 0 || stack[len(stack)-1] != '[' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+
+		case '}':
+			if len(stack) == 0 || stack[len(stack)-1] != '{' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+
+	}
+
+	return len(stack) == 0
+
 }
 
 type testCase struct {
